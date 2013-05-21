e752a65c5ecadba71664e73dc2504bfca683f761
jcf120 <jossie.cliffordfrith@gmail.com>
2013-05-21 19:58:18 +0100

    Added entry point and display code

--- Limbs/Windows Files/MainWindow.cpp
+++ Limbs/Windows Files/MainWindow.cpp
@@ -1,2 +1,13 @@
 #include "MainWindow.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+	Limbs::MainWindow form;
+    Application::Run(%form);
+}
