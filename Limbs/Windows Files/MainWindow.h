8ce3fd734287b0e8b6fc527298c242e390f20bbb
jcf120 <jossie.cliffordfrith@gmail.com>
2013-06-03 11:29:47 +0100

    Moved OpenGLControl project inside main solution.

--- Limbs/Windows Files/MainWindow.h
+++ Limbs/Windows Files/MainWindow.h
@@ -38,8 +38,9 @@ namespace Limbs {
 				delete components;
 			}
 		}
-	private: OpenGL::OpenGLControl^  openGLControl1;
+
 	private: System::Windows::Forms::Button^  button1;
+	private: OpenGL::OpenGLControl^  openGLControl1;
 	protected: 
 
 	protected: 
@@ -58,19 +59,9 @@ namespace Limbs {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->openGLControl1 = (gcnew OpenGL::OpenGLControl());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->openGLControl1 = (gcnew OpenGL::OpenGLControl());
 			this->SuspendLayout();
-			// 
-			// openGLControl1
-			// 
-			this->openGLControl1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
-				| System::Windows::Forms::AnchorStyles::Left));
-			this->openGLControl1->Location = System::Drawing::Point(0, 0);
-			this->openGLControl1->Name = L"openGLControl1";
-			this->openGLControl1->Size = System::Drawing::Size(240, 334);
-			this->openGLControl1->TabIndex = 0;
-			this->openGLControl1->Text = L"openGLControl1";
 			// 
 			// button1
 			// 
@@ -82,13 +73,23 @@ namespace Limbs {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MainWindow::button1_Click);
 			// 
+			// openGLControl1
+			// 
+			this->openGLControl1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->openGLControl1->Location = System::Drawing::Point(0, 0);
+			this->openGLControl1->Name = L"openGLControl1";
+			this->openGLControl1->Size = System::Drawing::Size(256, 332);
+			this->openGLControl1->TabIndex = 2;
+			this->openGLControl1->Text = L"openGLControl1";
+			// 
 			// MainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(504, 332);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->openGLControl1);
+			this->Controls->Add(this->button1);
 			this->Name = L"MainWindow";
 			this->Text = L"MainWindow";
 			this->ResumeLayout(false);
---
+++ OpenGL/OpenGL.h
@@ -0,0 +1,36 @@
+// OpenGL.h
+
+#pragma once
+
+#include <windows.h>
+#include <GL/gl.h>
+
+#using <System.dll>
+#using <System.Windows.Forms.dll>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+namespace OpenGL {
+
+	public ref class OpenGLControl : public System::Windows::Forms::Control
+	{
+
+	public:
+		OpenGLControl();
+
+		virtual Void Render(Void);
+
+		Void SwapOpenGLBuffers(Void);
+
+	private:
+		HDC m_hDC;
+		HGLRC m_hglrc;
+
+	protected:
+
+		~OpenGLControl();
+
+		GLint InternalSetPixelFormat(HDC hdc);
+	};
+}
